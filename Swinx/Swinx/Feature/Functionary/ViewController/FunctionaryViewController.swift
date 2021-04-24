7102f27581c0774685691f4163a39629e9c14781
BrunoMichelotti <brunomichelotti@hotmail.com>
2021-04-24 20:38:57 -0300

    Adding base FunctionaryViewController

--- Swinx/Swinx/Feature/Functionary/ViewController/FunctionaryViewController.swift
+++ Swinx/Swinx/Feature/Functionary/ViewController/FunctionaryViewController.swift
@@ -9,22 +9,27 @@
 import UIKit
 
 class FunctionaryViewController: UIViewController {
-
+    @IBOutlet weak var functionaryList: UITableView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.functionaryList?.delegate = self
+        self.functionaryList?.dataSource = self
     }
 
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension FunctionaryViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "popular com funcionario"
+        return cell
+    }
+    
+    
 }
