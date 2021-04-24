43b3f30f884e9eb0b379e832e21efc8b31287a67
Any <anyaspinheiro@gmail.com>
2021-04-24 16:18:30 -0300

    add json

---
+++ Swinx/Swinx/Feature/Base/ViewBase.swift
@@ -0,0 +1,9 @@
+//
+//  ViewBase.swift
+//  Swinx
+//
+//  Created by Any Ambria on 24/04/21.
+//  Copyright © 2021 Swinx. All rights reserved.
+//
+
+import Foundation
---
+++ Swinx/Swinx/Feature/Functionary/Model/FunctionaryModel.swift
@@ -0,0 +1,9 @@
+//
+//  FunctionaryModel.swift
+//  Swinx
+//
+//  Created by Any Ambria on 24/04/21.
+//  Copyright © 2021 Swinx. All rights reserved.
+//
+
+import Foundation
