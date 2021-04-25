ac3fce11a34fff010aa24dbacda28efdafa08fba
Any <anyaspinheiro@gmail.com>
2021-04-25 12:33:36 -0300

    adjust order cells detail

--- Swinx/Swinx/Feature/Functionary/ViewModel/DetailFunctionaryViewModel.swift
+++ Swinx/Swinx/Feature/Functionary/ViewModel/DetailFunctionaryViewModel.swift
@@ -24,17 +24,27 @@ class DetailFunctionaryViewModel {
         
         detailFunctionary.append(("Nome", functionaryData?.nome ?? "-", "", ""))
         detailFunctionary.append(("Aniversário", functionaryData?.aniversario ?? "-", "", ""))
+        detailFunctionary.append(("CPF", functionaryData?.cpf ?? "-", "RG", functionaryData?.rg ?? "-"))
+        detailFunctionary.append(("Cargo", functionaryData?.cargo ?? "-", "Matrícula Geco", functionaryData?.matriculaGeco ?? "-"))
         
-        detailFunctionary.append(("Data Inicio", functionaryData?.dataInicio ?? "-", "Data Fim", functionaryData?.dataFim ?? "-"))
-        detailFunctionary.append(("CPF", functionaryData?.cpf ?? "-", "", ""))
-        detailFunctionary.append(("RG", functionaryData?.rg ?? "-", "", ""))
-        detailFunctionary.append(("RACF", functionaryData?.racf ?? "-", "Funcional", functionaryData?.funcional ?? "-"))
+        detailFunctionary.append(("Gerente", functionaryData?.gerente ?? "-", "", ""))
+        detailFunctionary.append(("PL", functionaryData?.pl ?? "-", "", ""))
+        detailFunctionary.append(("TL", functionaryData?.tl ?? "-", "", ""))
+        detailFunctionary.append(("Ponto Focal", functionaryData?.pontoFocal ?? "-", "", ""))
         
-        detailFunctionary.append(("Cargo", functionaryData?.cargo ?? "-", "", ""))
+        detailFunctionary.append(("Celular Corporativo", functionaryData?.celularCorporativo ?? "-", "Telefone", functionaryData?.telefone ?? ""))
         detailFunctionary.append(("E-mail", functionaryData?.email ?? "-", "", ""))
+        detailFunctionary.append(("Data Inicio", functionaryData?.dataInicio ?? "-", "Data Fim", functionaryData?.dataFim ?? "-"))
+        
+        detailFunctionary.append(("Status", functionaryData?.status ?? "-", "", ""))
+        detailFunctionary.append(("RACF", functionaryData?.racf ?? "-", "Funcional", functionaryData?.funcional ?? "-"))
         detailFunctionary.append(("E-mail Cliente", functionaryData?.emailCliente ?? "-", "", ""))
+        detailFunctionary.append(("Squad", functionaryData?.squadProfissional ?? "-", "", ""))
+        detailFunctionary.append(("Código Projeto Jira", functionaryData?.codProjetoJira ?? "-", "PRE", functionaryData?.pre ?? ""))
+        
+        detailFunctionary.append(("Tecnologia", functionaryData?.tecnologia ?? "-", "Serviço", functionaryData?.servico ?? "-"))
+        detailFunctionary.append(("Shadow", functionaryData?.shadow ?? "-", "", ""))
         detailFunctionary.append(("Número MAC", functionaryData?.numeroMAC ?? "-", "", ""))
-        detailFunctionary.append(("Aniversário", functionaryData?.aniversario ?? "-", "", ""))
-        detailFunctionary.append(("Aniversário", functionaryData?.aniversario ?? "-", "", ""))
+        
     }
 }
